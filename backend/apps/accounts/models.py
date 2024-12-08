120467bbaaf19c5241e3c52cee98432ec8afe2bc
ahmedabougabal <ahmedabougabal@live.com>
2024-12-08 20:25:34 +0200

    feat: implements user model with the required fields adds a custom usermodel for extending django's abstract user class

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class UserRole(models.TextChoices):
+    ADMIN = 'ADMIN', 'admin'
+    MANAGER = 'MANAGER', 'manager'
+    EMPLOYEE = 'EMPLOYEE', 'employee'
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    role = models.CharField(
+        max_length=10,
+        choices= UserRole.choices,
+        default=UserRole.EMPLOYEE,
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'role']
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def __str__(self):
+        return self.email
+
