aa45e7418e03583565c79d87f71ad532962c49a6
ahmedabougabal <ahmedabougabal@live.com>
2024-12-08 23:57:01 +0200

    feat: Implements company model with custom validation and Calculated Properties for both employees and depts in each company
    - defines the Company model with fields: name, created_at, and updated_at.
    - implemented custom validation to raise an excepiton to ensure the name field is not empty.
    - Added calculated properties number_of_departments and number_of_employees to automatically compute the number of departments and employees associated with the company.
    - configured model metadata for verbose names and ordering by name.
    - overrides the __str__ dunder method to return the company's name

--- backend/apps/companies/models.py
+++ backend/apps/companies/models.py
@@ -1,3 +1,48 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+class Company(models.Model):
+    """
+    company model
+    - company name
+    - number of departments
+    - number of employees
+    """
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = "Companies"
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def number_of_departments(self):
+        """
+        automatically calculates number of departments
+        """
+        return self.departments.count()
+
+    @property
+    def number_of_employees(self):
+        """
+        automatically calculates number of employees
+        """
+        return sum(dept.number_of_employees for dept in self.departments.all())
+
+    def clean(self):
+        """
+        this should be for the custom validation as asked inside the pdf
+        --> validates all required fields are filled
+        """
+        if not self.name:
+            raise ValidationError({'name':'Company name cannot be empty'})
+
+
+
+
