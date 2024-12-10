94bb90d00eea163badb9f0c9294ed0f7fc21d1c5
ahmedabougabal <ahmedabougabal@live.com>
2024-12-10 11:53:41 +0200

    feat: registers company app's model into the admin interface by modifying admin.py

--- backend/apps/companies/admin.py
+++ backend/apps/companies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Company
 # Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number_of_departments', 'number_of_employees', 'created_at')
+    search_fields = ['name']
+    readonly_fields = ['created_at','updated_at']
