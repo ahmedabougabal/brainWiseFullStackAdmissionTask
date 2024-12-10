cdc1c09e6bf3ce425093428c87d89f9c8ecd743a
ahmedabougabal <ahmedabougabal@live.com>
2024-12-10 11:56:19 +0200

    feat: registers dept app's model into the admin interface by modifying admin.py

--- backend/apps/departments/admin.py
+++ backend/apps/departments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Department
 # Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'number_of_employees', 'created_at')
+    list_filter = 'company',
+    search_fields = ('name','company__name')
+    readonly_fields = ('created_at','updated_at')
