9727ec0cce21098105dd3e610d0b7af01040ee4c
ahmedabougabal <ahmedabougabal@live.com>
2024-12-13 18:05:06 +0200

    feat: Updates the AuthContextType interface to use signOut

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -4,7 +4,7 @@ import {authService} from "../services/auth.services";
 
 interface AuthContextType extends AuthState {
     login:(credentials: LoginCredentials) => Promise<void>;
-    logout:()=> void;
+    signOut:()=> void;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -45,26 +45,26 @@ export const AuthProvider: React.FC<{children: React.ReactNode}> = ({
           }));
       }
   }
-  const logout =() =>{
+  const signOut =() =>{
       authService.logout();
       setState({
           isAuthenticated:false,
           user:null,
-          loading: false,
-          error:null
+          loading:false,
+          error:null,
       });
-  };
+  }
   return (
-      <AuthContext.Provider value={{...state, login, logout}}>
+      <AuthContext.Provider value={{...state, login, signOut}}>
           {children}
       </AuthContext.Provider>
-  )
+  );
 };
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
-    if (context === undefined) {
-        throw new Error('useAuth must be used within a useAuthProvider');
+    if (!context) {
+        throw new Error("useAuth must be used within an AuthProvider");
     }
     return context;
 };
